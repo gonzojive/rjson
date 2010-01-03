76c29c9f2fb23f7b7358b47580f46a0a17ed641e
Red Daly <reddaly@gmail.com>
2010-01-02 20:26:39 -0800

    encoding CLOS objects

--- src/rjson-clos.lisp
+++ src/rjson-clos.lisp
@@ -1,5 +1,19 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; rjson-clos: utilities for sending CLOS instances across the net as RJSON objects
+
+;; Using RJSON's generic function-based protocol for defining RJSON `representations' of
+;; objects, we specialize REPRESENT-RJSON for standard-object.  We also add the generic
+;; function REPRESENT-RJSON-SLOT which takes an object and slot definition as arguments
+;; and returns the key and value for that slot being represented for that object.
+
+;; PSOS adds a powerful system for object-related programming in a web browser.
+;; It becomes even more powerful when we allow objects to be used interchangably
+;; in Lisp and Parenscript.  Not only must we conquer multi-language issues, but
+;; we must also deal with transmission of objects over a network.
+;; Controlling how classes are defined and shared between Lisp and Parenscript, 
 (in-package :rjson)
 ;;; This File Includes Utilities for sending CLOS instances across the net as
+
 (defmethod represent-rjson ((object structure-object))
   "Represents an object's slots and such in an RJSON-compatible way."
   (error "Cannot represent structures with RJSON."))
